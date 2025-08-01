402f95e28d4a149af35591afbc71c6aafe1a1cf2
jatin-senpai <yshake1004@gmail.com>
2025-08-01 07:34:52 +0000

    Update http and ws server configurations

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -7,16 +7,27 @@ const app = express()
 app.use(express.json())
 
 app.post("/signup",(req,res)=>{
-    const { userId, password } = req.body;
+    const data = UserSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.status(400).json({ error: "invalid inputs" });
+    }
+
     // Handle signup logic here
 
 
 })
 app.post("/signin",(req,res)=>{
+  const data = SignInSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.status(400).json({ error: "invalid inputs" });
+    }
     
 })
 app.post("/create-room",middleware,(req,res)=>{
-    
+    const data = RoomSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.status(400).json({ error: "invalid inputs" });
+    }
 })
 app.listen(3003, () => {
   console.log("Server is running on port 3003")
